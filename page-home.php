1ed8a2f145992790d0b740cca0ef45f6edc5df53
Matt Hummel <matt@matthummel.com>
2023-12-05 18:24:56 -0500

    remove latest projects, blog section

--- page-home.php
+++ page-home.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: Home Page */ ?>
 <?php get_header(); ?>
 
-  <?php get_template_part('template-parts/content', 'latest-projects'); ?>
-  <?php get_template_part('template-parts/content', 'latest-post');  ?>
+  <?php // get_template_part('template-parts/content', 'latest-projects'); ?>
+  <?php // get_template_part('template-parts/content', 'latest-post');  ?>
 
 <?php get_footer(); ?>
